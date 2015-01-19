60e2ecee50bfdc24fcff78cb9160d8b3edebf432
pavan gaikwad <pavan@pavans-MacBook-Air.local>
2015-01-19 14:57:51 +0530

    changed code formatting

--- keydriver.rb
+++ keydriver.rb
@@ -4,39 +4,39 @@ k.generate_keys
 scheduler = Rufus::Scheduler.new
 
 scheduler.every("5m") do
-	puts "keep alive cleanup in progress."
-	k.del_cleanup
+  puts "keep alive cleanup in progress."
+  k.del_cleanup
 end
 
 scheduler.every("1m") do
-	puts "release cleanup in progress."
-	k.release_cleanup
+  puts "release cleanup in progress."
+  k.release_cleanup
 end
 
 while true
-	print ">> "
-	cmd = gets.chomp
-	case cmd
-	when "dispatch" then puts k.dispatch_key
-	when "display" then puts k.used_map
-	when "all" then puts k.all_keys
-	when "delete" then
-			print "enter key >> "
-			key = gets.chomp
-			k.delete_key(key)
-	when "unblock" then  
-			print "enter key >> "
-			key = gets.chomp
-			if k.unblock_key(key)
-				puts "Unblocked #{key}"
-			else
-				puts "Unblock failed for key : #{key}"
-			end
-	when "keep" then
-			print "enter key >> "
-			key = gets.chomp
-			k.keep_alive(key)
+  print ">> "
+  cmd = gets.chomp
+  case cmd
+  when "dispatch" then puts k.dispatch_key
+  when "display" then puts k.used_map
+  when "all" then puts k.all_keys
+  when "delete" then
+    print "enter key >> "
+    key = gets.chomp
+    k.delete_key(key)
+  when "unblock" then
+    print "enter key >> "
+    key = gets.chomp
+    if k.unblock_key(key)
+      puts "Unblocked #{key}"
+    else
+      puts "Unblock failed for key : #{key}"
+    end
+  when "keep" then
+    print "enter key >> "
+    key = gets.chomp
+    k.keep_alive(key)
 
-	else puts "did not understand."	
-	end
+  else puts "did not understand."
+  end
 end
--- spec/key_server_spec.rb
+++ spec/key_server_spec.rb
@@ -1,7 +1,7 @@
 require_relative "../key-server"
 
 describe KeyServer do
-  it "should create an instance of the key_server" do 
+  it "should create an instance of the key_server" do
     @key_server = KeyServer.new
     expect(@key_server).to be_instance_of(KeyServer)
   end
@@ -19,7 +19,7 @@ describe KeyServer do
     expect(@key_server.dispatch_key).to_not eq(nil)
   end
 
-  it "should return only 10 keys in default case" do 
+  it "should return only 10 keys in default case" do
     @key_server = KeyServer.new
     @key_server.generate_keys
     # 10 keys should be returned
@@ -30,7 +30,7 @@ describe KeyServer do
     expect(@key_server.dispatch_key).to eq(nil)
   end
 
-  it "should return n number of keys in non-default behaviour" do 
+  it "should return n number of keys in non-default behaviour" do
     n = 16
     @key_server = KeyServer.new
     @key_server.generate_keys(number=n)
@@ -41,7 +41,4 @@ describe KeyServer do
     # (n+1)th key must be nil
     expect(@key_server.dispatch_key).to eq(nil)
   end
-
-  
-
 end
